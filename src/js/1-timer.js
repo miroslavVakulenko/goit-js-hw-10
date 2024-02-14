eeedf384cc7c8d7bb4f1e3a44780f44b05aa497e
vanger228 <112012329+vanger228@users.noreply.github.com>
2024-02-14 21:20:43 +0200

    2

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,13 +1,21 @@
 import flatpickr from 'flatpickr';
 
-const options = {
-  enableTime: true,
-  time_24hr: true,
-  defaultDate: new Date(),
-  minuteIncrement: 1,
-  onClose(selectedDates) {
-    console.log(selectedDates[0]);
-  },
+const timeHandler = input => {
+  let selectedUserDate = {};
+  const options = {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    minDate: Date.now(),
+    onClose(selectedDates) {
+      selectedUserDate = selectedDates[0];
+    },
+  };
+  flatpickr(input, options);
+  console.log(selectedUserDate);
 };
 
-flatpickr('#datetime-picker', options);
+const input = document.querySelector('#datetime-picker');
+
+timeHandler(input);
