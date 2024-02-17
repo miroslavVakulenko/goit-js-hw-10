319807d506d5740dc46b80b8cef84d9872ee2f89
vanger228 <112012329+vanger228@users.noreply.github.com>
2024-02-17 13:09:20 +0200

    3

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,36 @@
+'use strict';
+
+import iziToast from 'izitoast';
+
+const popupHandler = (delay, state) => {
+  const promise = new Promise((res, rej) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        res(`✅ Fulfilled promise in ${delay}ms`);
+      } else {
+        rej(`❌ Rejected promise in ${delay}ms`);
+      }
+    }, delay);
+  });
+  promise
+    .then(res => {
+      console.log(res);
+    })
+    .catch(rej => {
+      console.log(rej);
+      iziToast.error({
+        title: 'Error',
+        message: 'Illegal operation',
+      });
+    });
+};
+
+const initForm = () => {
+  const form = document.querySelector('.form');
+  form.addEventListener('submit', evt => {
+    evt.preventDefault();
+    popupHandler(form.delay.value, form.state.value);
+  });
+};
+
+initForm();
