53ff3ba57d7e02b303b6c3d5d349c6741105ef6a
vanger228 <112012329+vanger228@users.noreply.github.com>
2024-02-18 10:22:28 +0200

    svg error

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -2,26 +2,44 @@
 
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
+import success from '../img/iconmonstr-check-mark-14.svg';
+import error from '../img/iconmonstr-error-8.svg';
 
 const popupHandler = (delay, state) => {
   const promise = new Promise((res, rej) => {
     setTimeout(() => {
       if (state === 'fulfilled') {
-        res(`✅ Fulfilled promise in ${delay}ms`);
+        res(`Fulfilled promise in ${delay}ms`);
       } else {
-        rej(`❌ Rejected promise in ${delay}ms`);
+        rej(`Rejected promise in ${delay}ms`);
       }
     }, delay);
   });
   promise
     .then(res => {
-      console.log(res);
+      iziToast.success({
+        class: 'popup-message',
+        theme: 'dark',
+        backgroundColor: '#7fffd4',
+        messageColor: '#fff',
+        iconUrl: success,
+        position: 'topRight',
+        pauseOnHover: true,
+        timeout: 3000,
+        message: res,
+      });
     })
     .catch(rej => {
-      console.log(rej);
       iziToast.error({
-        title: 'Error',
-        message: 'Illegal operation',
+        class: 'popup-message',
+        theme: 'dark',
+        backgroundColor: '#ef4040',
+        messageColor: '#fff',
+        iconUrl: error,
+        position: 'topRight',
+        pauseOnHover: true,
+        timeout: 3000,
+        message: rej,
       });
     });
 };
